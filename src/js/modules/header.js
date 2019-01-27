bd1cfdc5d2aa36eee030e5a7f309d416d44ba476
gothaw <rad.soltan@gmail.com>
2019-01-27 22:01:21 +0000

    added mobile styling and js and responsive font

--- src/js/modules/header.js
+++ src/js/modules/header.js
@@ -3,10 +3,46 @@ import enquire from '../lib/enquire.js'
 (function () {
 
     //jQuery variables
-    const $menuLogo         = $('.menu__logo');
-    const $menu             = $('.menu');
+    const $menuLogo                 = $('.menu__logo');
+    const $menuItems                = $('.menu__item');
+    const $toggleMenuIconImg        = $('.toggle__img');
 
+    let toggleMenuActive = false;
 
+    /**
+     *
+     */
+    function registerMenu() {
+        enquire.register("screen and (max-width: 767px)", {
+            match: function () {
+                $menuItems.hide();
+                $toggleMenuIconImg.attr("src","dist/img/icons/toggle-icon.png");
+                toggleMenuActive=false;
+            },
+            unmatch: function () {
+                $menuItems.show();
+            }
+        });
+    }
+
+    /**
+     *
+     */
+    function toggleMenu() {
+        $menuItems.slideToggle();
+        if(toggleMenuActive){
+            $toggleMenuIconImg.attr("src","dist/img/icons/toggle-icon.png");
+            toggleMenuActive=false;
+        }
+        else {
+            $toggleMenuIconImg.attr("src","dist/img/icons/toggle-icon-active.png");
+            toggleMenuActive=true;
+        }
+    }
+
+    /**
+     *
+     */
     function responsiveLogo() {
         enquire.register("screen and (min-width: 1025px)", {
             match: function () {
@@ -19,12 +55,13 @@ import enquire from '../lib/enquire.js'
     }
 
     function eventHandler() {
-
+        $toggleMenuIconImg.on('click',toggleMenu);
     }
 
     function init() {
         eventHandler();
         responsiveLogo();
+        registerMenu();
     }
 
     window.addEventListener("load", init);
