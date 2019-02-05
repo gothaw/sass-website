c765e0d160193a90ab9049aa349a26ff593755f6
gothaw <rad.soltan@gmail.com>
2019-02-05 23:53:20 +0000

    added function to animate the typing cursor

--- src/js/modules/header.js
+++ src/js/modules/header.js
@@ -6,11 +6,20 @@ import enquire from '../lib/enquire.js'
     const $menuLogo                 = $('.menu__logo');
     const $menuItems                = $('.menu__item');
     const $toggleMenuIconImg        = $('.toggle__img');
+    const $textCursor               = $('.header__typing-animation');
 
     let toggleMenuActive = false;
 
+    function typingAnimation() {
+        setInterval(function () {
+            $textCursor.toggleClass('header__typing-animation--active')
+        },500)
+    }
+    
     /**
-     *
+     * @name        registerMenu
+     * @desc        Function hides the menu bar links when width is less than 767px and shows when it is greater than 767px.
+     *              Uses enquire.js module.
      */
     function registerMenu() {
         enquire.register("screen and (max-width: 767px)", {
@@ -26,7 +35,8 @@ import enquire from '../lib/enquire.js'
     }
 
     /**
-     *
+     * @name        toggleMenu
+     * @desc        Slide toggle animation for hamburger menu. Changes src attribute for menu icon image when icon is clicked.
      */
     function toggleMenu() {
         $menuItems.slideToggle();
@@ -41,7 +51,9 @@ import enquire from '../lib/enquire.js'
     }
 
     /**
-     *
+     * @name        responsiveLogo
+     * @desc        Function adds img-responsive class to the image if screen width is greater than 1025px.
+     *              Uses enquire.js.
      */
     function responsiveLogo() {
         enquire.register("screen and (min-width: 1025px)", {
@@ -62,6 +74,7 @@ import enquire from '../lib/enquire.js'
         eventHandler();
         responsiveLogo();
         registerMenu();
+        typingAnimation();
     }
 
     window.addEventListener("load", init);
