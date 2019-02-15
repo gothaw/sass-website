b897da5cd8b822747a09051f6cd850bf2cbdc73c
gothaw <rad.soltan@gmail.com>
2019-02-15 22:01:02 +0000

    simplified code for parallax function

--- src/js/index.js
+++ src/js/index.js
@@ -3,18 +3,23 @@ import enquire from './lib/enquire.js'
 (function () {
     //jQuery variables
     const $aboutImg           = $('.about__img');
-
+    const $spacerImg          = $('.spacer__img');
 
     //variables
     const skills              = document.getElementById('skills');
     const skillBars           = document.querySelectorAll('.skill__bar');
-    let imgPercent;
-    let parallaxInterval;
 
     /**
-     *
+     * @name        spacerImgParallax
+     * @desc        Function handles parallax effect for spacer images. It uses setInterval which updates the top and transform: translateY properties of the image.
+     *              The parallax effect is displayed only for window width greater than 1025px using enquire.js. For window width less than 1025px, the image properties are restored to default values.
+     * @param       speed - speed of the parallax effect (between 0 and 1)
+     * @param       $img - jQuery array like object of spacer images
+     * @param       $wrapper - jQuery array like object of spacer image wrappers
      */
     function spacerImgParallax(speed,$img,$wrapper) {
+        let parallaxInterval;
+        let imgPercent;
         enquire.register("screen and (min-width: 1025px)", {
             match: function () {
                 parallaxInterval = setInterval(function () {
@@ -22,28 +27,22 @@ import enquire from './lib/enquire.js'
                     const winH          = $(this).height();
                     const winBottom     = winTop + winH;
                     const wrapperTop    = $wrapper.offset().top;
-                    const wrapperH      = $wrapper.height();
-                    const wrapperBottom = wrapperTop + wrapperH;
+                    const wrapperH      = $wrapper.innerHeight();
 
-                    // If block is shown on screen
-                    if (winBottom > wrapperTop && winTop < wrapperBottom) {
-                        // Number of pixels shown after block appear
-                        const imgBottom = ((winBottom - wrapperTop) * speed);
-                        // Max number of pixels until block disappear
-                        const imgTop = winH + wrapperH;
-                        // Percentage between start showing until disappearing
-                        imgPercent = ((imgBottom / imgTop) * 100) + (50 - (speed * 50));
-                    }
+                    const imgBottom = ((winBottom - wrapperTop) * speed);
+                    const imgTop = winH + wrapperH;
+                    imgPercent = ((imgBottom / imgTop) * 100) + (50 - (speed * 50));
+
                     $img.css({
                         top: imgPercent + '%',
-                        transform: 'translate(-50%, -' + imgPercent + '%)'
+                        transform: 'translateY(-' + imgPercent + '%)'
                     });
-                },1000/120);
+                },1000/60);
             },
             unmatch: function () {
                 $img.css({
                     top: '0',
-                    transform: 'translate(-50%, 0)'
+                    transform: 'translateY(0)'
                 });
                 clearInterval(parallaxInterval);
             }
@@ -67,6 +66,22 @@ import enquire from './lib/enquire.js'
     }
 
     /**
+     * @name        responsiveSpacerImg
+     * @desc        Function adds img-responsive class to the image if screen width is greater than 540px.
+     *              Uses enquire.js.
+     */
+    function responsiveSpacerImg() {
+        enquire.register("screen and (min-width: 540px)", {
+            match: function () {
+                $spacerImg.addClass("img-responsive");
+            },
+            unmatch: function () {
+                $spacerImg.removeClass("img-responsive");
+            }
+        });
+    }
+
+    /**
      * @name        responsiveAboutImg
      * @desc        Function adds img-responsive class to the image if screen width is less than 768px.
      *              Uses enquire.js.
@@ -81,19 +96,20 @@ import enquire from './lib/enquire.js'
             }
         });
     }
+
     function eventHandler() {
         $('.spacer__img').each(function () {
             const $spacerImg          = $(this);
             const $spacerImgWrapper   = $(this).parent();
-            spacerImgParallax(0.75,$spacerImg,$spacerImgWrapper);
+            spacerImgParallax(0.4,$spacerImg,$spacerImgWrapper);
         });
     }
 
     function init() {
         eventHandler();
         responsiveAboutImg();
+        responsiveSpacerImg();
         animateSkillBars();
-        // spacerImgParallax(0.75);
     }
 
     window.addEventListener("load", init);
